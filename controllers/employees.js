d27bea16786eb0c722d4187f16df804215d45eb2
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2023-05-30 22:39:34 +0300

    230

--- controllers/employees.js
+++ controllers/employees.js
@@ -23,20 +23,81 @@ const add = async (req, res) => {
     if (!data.firstName || !data.lastName || !data.address || !data.age) {
       return res.status(400).json({ message: "All fields are required" });
     }
-    const employee = await prisma.user.update({
-      where: {
-        id: req.user.id,
-      },
-      data: {
-        createdEmployee: {
-          create: data,
-        },
-      },
+
+    const employee = await prisma.employee.create({
+      data: { ...data, userId: req.user.id },
     });
-    return res.status(201).json({ message: "employee is updated //add" });
+    return res.status(201).json({ message: `${employee} is create` });
   } catch (error) {
-    res.status(400).json({ message: "fault duging add  employee // Add" });
+    res.status(400).json({ message: "fault during add  employee // Add" });
+  }
+};
+/**
+ * @route POST api/employees/remove/:id
+ * @desc удалить сотрудника
+ * @access Private
+ */
+const remove = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({ message: "add a employee ID" });
+    }
+    const removedEmployee = await prisma.employee.delete({
+      where: { id: id },
+    });
+    return res
+      .status(201)
+      .json({ message: `user with id ${id} has been deleted` });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "fault during remove  employee // remove" });
+  }
+};
+/**
+ * @route POST api/employees/edit/:id
+ * @desc изменить данные сотрудника
+ * @access Private
+ */
+const edit = async (req, res) => {
+  try {
+    const data = req.body;
+    const id = data.id;
+    if (
+      !data.firstName ||
+      !data.lastName ||
+      !data.address ||
+      !data.age ||
+      !data.id
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const employee = await prisma.employee.update({
+      where: {
+        id,
+      },
+      data,
+    });
+    return res.status(201).json({ message: ` ${data.id} is edit` });
+  } catch (error) {
+    res.status(400).json({ message: "fault during edit  employee // edit" });
   }
 };
 
-module.exports = { all, add };
+/**
+ * @route GET api/employees/:id
+ * @desc получить данные сотрудника
+ * @access Private
+ */
+const employee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await prisma.employee.findUnique({ where: { id: id } });
+    console.log(employee);
+    return res.status(201).json({ message: `user ${employee} arrived` });
+  } catch (error) {
+    res.status(400).json({ message: "fault duging get employee // Employee" });
+  }
+};
+module.exports = { all, add, remove, remove, edit, employee };
--- routes/employees.js
+++ routes/employees.js
@@ -2,20 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 const { auth } = require("../middleware/auth");
-const { all, add } = require("../controllers/employees");
+const {
+  all,
+  add,
+  remove,
+  edit,
+  employee,
+} = require("../controllers/employees");
 // req - доступ к Header, params
 // res - доступ к ответу сервера
 
 // api/employees
 router.get("/", auth, all);
 // api/employees:id
-router.get("/:id", auth, () => {});
+router.get("/:id", auth, employee);
 //api/employees/add
 router.post("/add", auth, add);
 //api/employees/remove/:id
-router.post("/remove/:id", auth, () => {});
+router.post("/remove/:id", auth, remove);
 //api/employees/edit/:id
-router.put("/edit/:id", auth, () => {});
+router.put("/edit/:id", edit);
 //
 //
 module.exports = router;
