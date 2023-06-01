e76f4fd1286daa7f1a2a9ed2b95c9d825bcf081b
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2023-06-01 21:40:22 +0300

    525

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -6,26 +6,42 @@ import PasswordInput from "../../components/password-input/PasswordInput";
 import { CustomButton } from "../../components/custom-button/CustomButton";
 import { Link } from "react-router-dom";
 import { Paths } from "../../paths";
+import { UserData, useLoginMutation } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-err-with-message";
+import ErrorMessage from "../../components/error-message/ErrorMessage";
 
 const Login = () => {
+  const [loginUser, loginUserResult] = useLoginMutation();
+  const [error, setError] = React.useState("");
+  const onClickLogin = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
+    } catch (error) {
+      const maybeError = isErrorWithMessage(error);
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError("unknown error");
+      }
+    }
+  };
   return (
     <Layout>
       <Row align={"middle"} justify={"center"}>
         <Card title={"enter"} style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={onClickLogin}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Password" />
 
             <CustomButton htmlType={"submit"} type="primary">
-              {" "}
-              to log in{" "}
+              to log in
             </CustomButton>
           </Form>
           <Space direction={"vertical"} size={"large"}></Space>
           <Typography.Text>
-            {" "}
             Havent Acc? <Link to={Paths.register}> Create account</Link>
           </Typography.Text>
+          <ErrorMessage message={error} />
         </Card>
       </Row>
     </Layout>
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,7 +1,36 @@
-import React from "react";
-
+import Layout from "../../components/layout/Layout";
+import { Card, Form, Input, Row, Space, Typography } from "antd";
+import CustomInput from "../../components/custom-input/CustomInput";
+import PasswordInput from "../../components/password-input/PasswordInput";
+import { CustomButton } from "../../components/custom-button/CustomButton";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Layout>
+      <Row align={"middle"} justify={"center"}>
+        <Card title={"enter"} style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput type="text" name="name" placeholder="Name" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Password" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Repeat password"
+            />
+            <CustomButton htmlType={"submit"} type="primary">
+              Create account
+            </CustomButton>
+          </Form>
+          <Space direction={"vertical"} size={"large"}></Space>
+          <Typography.Text>
+            {" "}
+            Do you have Account? <Link to={Paths.login}> Log in</Link>
+          </Typography.Text>
+        </Card>
+      </Row>
+    </Layout>
+  );
 };
 
 export default Register;
